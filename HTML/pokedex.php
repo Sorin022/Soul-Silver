7d9ffed83041e7b98fff875817e66b0cfb6317da
Sorin Macaluso <sorinmagnusmacaluso@gmail.com>
2022-10-22 18:51:46 -0400

    update 3

--- HTML/pokedex.php
+++ HTML/pokedex.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -8,6 +9,11 @@
         <link rel="stylesheet" href="assets/SS.css">
 
         <link rel="shortcut icon" href="assets/img/Poké_Ball_icon.svg.png" type="image/png">
+
+        <style>
+            small {font-size: 5}
+        </style>
+
     </head>
     <body>
         <div id = "header">
@@ -15,12 +21,17 @@
                 <a class= "active" href="pokemon.html"> Home</a>
                 <a href="pokedex.php"> Pokedex</a>
                 <a href="BattleSim.html"> Battle Simulator</a>
+                <a href="Feedback.html"> Feedback</a>
+                <a href="Disclaimer.html"> Disclaimer </a>
+                <a href="mailto: Collin.Drake1@marist.edu"> Contact Us</a>
             </div>
         </div>
+
+        
         <?php
             $servername = "localhost";
-            $username = "xperiencedbum";
-            $password = "CollinDrake";
+            $username = "root";
+            $password = "password";
             $dbname = "pokedex_db";
 
             // Create connection
@@ -39,7 +50,7 @@
                 if ($result->num_rows > 0) {
                     // output data of each row
                     while($row = $result->fetch_assoc()) {
-                    echo "<h3>"."Dex Number: " . $row["dex_num"]. " | Pokemon Name: " . $row["poke_name"]. " | Pokemon Type 1: " . $row["poke_type1"] . " | Pokemon Type 2: " . $row["poke_type2"] . " | Pokemon Move Name: " . $row["poke_move_name"]. " | Pokemon Move type: " . $row["poke_move_type"]. " | Pokemon Move Damage: " . $row["poke_move_dmg"]. "<h3>" . "<br>";
+                    echo "<small><b> Dex Number: " . $row["dex_num"]. " | Pokemon Name: " . $row["poke_name"]. " | Pokemon Move Name: " . $row["poke_move_name"]. " | Pokemon Move Damage: " . $row["poke_move_dmg"]. "</small>" . "<br>";
                     echo "<hr>";
                     }
                 } else {
@@ -47,23 +58,8 @@
                 }
                 $conn->close();
 
-        ?>
+        ?> 
 
-
-        <footer data-role="footer">
-        
-            <div id = "footer">
-                <img src="assets/img/pokemon-logo-black-transparent.png" alt="x">
-                <br>
-                <p> (C) Pokemon.html is property of its respected owners and collaborators. </p>
-                <p> Headquarters are in Marist College Poughkeepsie </p>
-                <div class="bottomnav">
-                    <a href="mailto: Collin.Drake1@marist.edu"> Contact Us</a>
-                    <a href="Feedback.html"> Feedback</a>
-                    <a href="Disclaimer.html"> Disclaimer </a>
-                </div>
-            </div>
-        </footer>
     </body>
     
 </html>
